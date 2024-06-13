932709933f5e75082995800052df670c94ae4cfc
nyoung <dlagusdyd347398@daum.net>
2024-06-13 11:51:26 +0900

    초기 설정

---
+++ view/src/test/java/com/jamjam/view/ViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamjam.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
