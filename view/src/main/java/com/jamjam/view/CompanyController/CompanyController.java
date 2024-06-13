4474ee30180b3d859155e88b4b044d03786106b7
jun <wnsgud0202@naver.com>
2024-06-13 14:19:19 +0900

    기업회원가입 작업중

---
+++ view/src/main/java/com/jamjam/view/CompanyController/CompanyController.java
@@ -0,0 +1,11 @@
+package com.jamjam.view.CompanyController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/company")
+public class CompanyController {
+
+	
+}
