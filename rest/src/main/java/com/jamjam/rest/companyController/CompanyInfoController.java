d6d34c10638193f83b18a302d45316da227c6a5c
jun <wnsgud0202@naver.com>
2024-06-18 11:14:37 +0900

    기업소개 삭제 기능 완료

--- rest/src/main/java/com/jamjam/rest/companyController/CompanyInfoController.java
+++ rest/src/main/java/com/jamjam/rest/companyController/CompanyInfoController.java
@@ -2,10 +2,12 @@ package com.jamjam.rest.companyController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,19 @@ public class CompanyInfoController {
 		
 	}
 	
+	@PutMapping("/companyInfo")
+	public ResponseEntity<?> updateCompanyinfoImage(@ModelAttribute CompanyInfoDto companyInfoDto) {
+		System.out.println("컨트롤러 들어옴");
+		System.out.println(companyInfoDto);
+		String result = companyInfoService.updateCompanyInfoImage(companyInfoDto);
+		if(result.contains("성공")) {
+			return ResponseEntity.ok().body(result);
+		}else {
+			return  ResponseEntity.status(500).body(result);
+		}
+		
+	}
+	
 	@GetMapping("/companyInfo/{user_id}")
 	public CompanyInfo getCompanyInfo(@PathVariable("user_id") Integer user_id) {
 		System.out.println("회사소개get 들어옴");
@@ -41,6 +56,13 @@ public class CompanyInfoController {
 		System.out.println(result); 
 		
 		return result;
+	}
+	
+	@DeleteMapping("/companyInfo/{info_id}")
+	public void deleteCompanyInfo(@PathVariable("info_id") Integer info_id) {
+		System.out.println("회사소개delete 들어옴");
+		companyInfoService.deleteCompanyInfo(info_id);
+		
 	}
 	@GetMapping("/companyInfo/info_id/{info_id}")
 	public CompanyInfo getCompanyInfoByInfoId(@PathVariable("info_id") Integer info_id) {
@@ -50,4 +72,6 @@ public class CompanyInfoController {
 		
 		return result;
 	}
+	
+	
 }
--- rest/src/main/java/com/jamjam/rest/service/Company/CompanyInfoService.java
+++ rest/src/main/java/com/jamjam/rest/service/Company/CompanyInfoService.java
@@ -60,10 +60,59 @@ public class CompanyInfoService {
 		
 	}
 	
+	public String updateCompanyInfoImage(CompanyInfoDto companyInfoDto) {
+		CompanyInfo companyInfo = new CompanyInfo();
+		companyInfo.setCompany_id(companyInfoDto.getCompany_id());
+		companyInfo.setDescription(companyInfoDto.getDescription());
+		companyInfo.setNumber_of_employees(companyInfoDto.getNumber_of_employees());
+		companyInfo.setWorking_environment(companyInfoDto.getWorking_environment());
+		companyInfo.setBenefits(companyInfoDto.getBenefits());
+		companyInfo.setIdeal_candidate(companyInfoDto.getIdeal_candidate());
+		companyInfo.setMain_contact(companyInfoDto.getMain_contact());
+		companyInfo.setMain_email(companyInfoDto.getMain_email());
+		//파일 관련 멤버변수 세팅
+			
+		if(!(companyInfoDto.getFileName() ==null ||companyInfoDto.getFileName()=="")) {
+				String originName =companyInfoDto.getFileName();
+				companyInfo.setOrigin_name(originName);
+				String newName = UUID.randomUUID().toString() + originName;//중복되지 않는 새이름
+				companyInfo.setNew_name(newName);
+				//String thumbNailName = "";
+		File file = new File(newName);
+		
+		try {
+			companyInfoDto.getFile().transferTo(file);
+			System.out.println("파일 업로드 성공....");
+			
+			//썸네일 생성
+			//String thumbnailSaveName = "s_" + newName;
+			//board.setThumbnailName(thumbnailSaveName);
+			
+			//File thumbfile = new File(uploadPath + thumbnailSaveName);
+			File ufile = new File(uploadPath + newName);
+			
+			//Thumbnails.of(ufile).size(100,100).toFile(thumbfile);
+			System.out.println(companyInfo);
+			companyInfoMapper.updateCompanyInfoImage(companyInfo);
+			return "등록성공";
+		}catch(Exception e){
+			return "등록실패" + e.getMessage();
+		}
+		}else {
+			companyInfoMapper.updateCompanyInfo(companyInfo);
+			return "등록성공";
+		}
+	}
+	
 	public CompanyInfo getCompanyInfo(Integer compnay_id) {
 		return companyInfoMapper.getCompanyInfo(compnay_id);
 	}
 	public CompanyInfo getCompanyInfoByInfo_id(Integer info_id) {
 		return companyInfoMapper.getCompanyInfoByInfoId(info_id);
 	}
+	
+	public void deleteCompanyInfo(Integer info_id) {
+		companyInfoMapper.deleteCompanyInfo(info_id);
+	}
+	
 }
