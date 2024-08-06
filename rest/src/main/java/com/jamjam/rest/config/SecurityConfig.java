f2f7920f13cef11b1bc07667073fddf85acc2e05
ny0ung0 <dlagusdyd347398@daum.net>
2024-08-06 09:09:06 +0900

    주석추가

--- rest/src/main/java/com/jamjam/rest/config/SecurityConfig.java
+++ rest/src/main/java/com/jamjam/rest/config/SecurityConfig.java
@@ -19,30 +19,26 @@ import jakarta.servlet.http.HttpServletResponse;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	
-	
-	
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.disable()) // CSRF 보호를 비활성화합니다.
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/", "signup", "/oauth2/**", "/login/**", "/error").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().permitAll() // 나머지 모든 요청을 허용합니다.
             )
             .oauth2Login(oauth2 -> oauth2
-                .defaultSuccessUrl("http://localhost:9999/usermain", true)
-                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("http://localhost:9999/usermain", true) // 로그인 성공 시 리디렉션 URL을 설정합니다.
+                .failureUrl("/login?error=true") // 로그인 실패 시 리디렉션 URL을 설정합니다.
                 .userInfoEndpoint(userInfo -> userInfo
-                    .userService(oauth2UserService())
+                    .userService(oauth2UserService()) // 사용자 정보를 가져오기 위한 서비스 설정
                 )
             )
             .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("http://localhost:9999/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "XSRF-TOKEN")
+                .logoutUrl("/logout") // 로그아웃 URL을 설정합니다.
+                .logoutSuccessUrl("http://localhost:9999/") // 로그아웃 성공 시 리디렉션 URL을 설정합니다.
+                .invalidateHttpSession(true) // 세션을 무효화합니다.
+                .deleteCookies("JSESSIONID", "XSRF-TOKEN") // 특정 쿠키를 삭제합니다.
                 .clearAuthentication(true) // 인증 정보 제거
                 .addLogoutHandler((request, response, authentication) -> {
                     // 로그아웃 시 Google 로그아웃 URL로 리디렉션
@@ -56,11 +52,11 @@ public class SecurityConfig {
                 })
             );
         
-        return http.build();
+        return http.build(); // SecurityFilterChain 빌드
     }
 
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
-        return new CustomOAuth2UserService();
+        return new CustomOAuth2UserService(); // 커스텀 OAuth2UserService를 반환
     }
 }
